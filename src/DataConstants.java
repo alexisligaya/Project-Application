e43fb61c0e7b360927bcf8d5812673c6288b4966
Casey <caseyvu77@gmail.com>
2023-10-15 17:55:03 -0400

    save

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -1,3 +1,16 @@
-public class DataConstants {
+public abstract class DataConstants {
     
+    protected static final String USER_ID = "userID";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_USERNAME = "userName";
+    protected static final String USER_EMAIL = "email";
+    protected static final String USER_PASSWORD = "password";
+    protected static final String USER_DOB = "dateOfBirth";
+    protected static final String USER_TASKS = "tasks";
+
+
+
+
+
 }
