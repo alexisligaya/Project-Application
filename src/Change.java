6104949381806bdf05adbf26a95546f6c1eceafe
Casey <caseyvu77@gmail.com>
2023-10-31 14:17:55 -0400

    columns javadoc

--- src/Change.java
+++ src/Change.java
@@ -1,20 +1,21 @@
 import java.util.Date;
 
 public class Change {
-    
+
     private String description;
     private Date date;
     private User user;
     private Project project;
 
     /**
-     * Creates a Change object with the given information:
+     * Constructor of a Change object with the given parameters
+     * 
      * @param description - description of the change made
-     * @param date - date of when the change was made
-     * @param user - the user responsible for the change
-     * @param project - the current project whose information is being changed
+     * @param date        - date of when the change was made
+     * @param user        - the user responsible for the change
+     * @param project     - the current project whose information is being changed
      */
-    public Change(String description, Date date, User user, Project project){
+    public Change(String description, Date date, User user, Project project) {
         this.description = description;
         this.date = date;
         this.user = user;
@@ -23,43 +24,48 @@ public class Change {
 
     /**
      * Gets the description of the change
+     * 
      * @return a string of the description for the change made
      */
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
-    
+
     /**
      * Gets the date of the change
+     * 
      * @return a date of when the change was made
      */
-    public Date getDate(){
+    public Date getDate() {
         return date;
     }
 
     /**
      * Gets the user who made the change
+     * 
      * @return a user object
      */
-    public User getUser(){
+    public User getUser() {
         return user;
     }
 
     /**
      * Gets the project that was changed
+     * 
      * @return a project object
      */
-    public Project getProject(){
+    public Project getProject() {
         return project;
     }
 
     /**
      * Formats the string of all the information for the change
+     * 
      * @return a formatted string
      */
-    public String toString(){
+    public String toString() {
         String result = "Decription: " + this.description;
-        result += "\nDate: "+this.date;
+        result += "\nDate: " + this.date;
         result += "\nUser: " + this.user;
         result += "\nProject: " + this.project;
         return result;
