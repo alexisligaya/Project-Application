eece609ce0202d45afe9dc299ae826c1a0557309
alexisligaya <alp12@email.sc.edu>
2023-11-02 14:12:14 -0400

    javaTest classes assigned

---
+++ src/JavaTest.java
@@ -0,0 +1,12 @@
+//Alexis- DataWriter, User, UserList, Application
+
+
+//Marietou- DataLoader, DataConstants, Columns, Tasks
+
+
+
+//Casey- Project, ProjectHistory, ProjectList, Comments, 
+
+
+
+//Laura- Team, Change, Dashboard 
