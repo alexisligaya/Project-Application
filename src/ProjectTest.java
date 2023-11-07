8811c97a3f81819f245e5986e0f06655bb85a9a8
Casey <caseyvu77@gmail.com>
2023-11-07 15:36:40 -0500

    caseys testing

--- src/ProjectTest.java
+++ src/ProjectTest.java
@@ -63,6 +63,7 @@ public class ProjectTest{
 
 
 //Casey- Project, ProjectHistory, ProjectList, Comments, 
+@Test
 public void testAddTasks() {
 
     ProjectList projectList = ProjectList.getInstance();
@@ -78,17 +79,118 @@ public void testAddTasks() {
     Project newProject = new Project("Flappy Bird", "Make the bird go through pipes for as long as possible");
     projectList.addProject(newProject);
     
-    ArrayList<Project> projects = projectList.getProjects();
-
     newProject.addTasks(deadline, taskDescription, priority, hours, assignedUser, changes, comments);
 
     ArrayList<Tasks> firstColumnTasks = newProject.getColumns().get(0).getTasks();
     Tasks addedTask = firstColumnTasks.get(0);
 
     assertEquals(taskDescription, addedTask.getTaskDescription());
-    assertEquals(assignedUser, addedTask.getAssignedUser());
 }
 
+@Test
+public void testAddMember() {
+
+    Project newProject = new Project("Flappy Bird", "Make the bird go through pipes for as long as possible");
+    User user = new User("Casey", "Vu", "CaseyVu", "cv10@email.sc.edu", "cv10", "Microsoft", new Date());
+
+    newProject.addMember(user);
+
+    newProject.addMember(user); //do it again to test for dupes
+
+    ArrayList<User> members = newProject.getMembers();
+
+    assertEquals(1, members.size());
+}
+
+@Test
+public void testAddColumns() {
+
+    Project newProject = new Project("Flappy Bird", "Make the bird go through pipes for as long as possible");
+    String title = "Sample Column";
+
+    newProject.addColumns(title);
+    ArrayList<Columns> columns = newProject.getColumns();
+    Columns addedColumn = columns.get(0);
+
+    assertEquals(title, addedColumn.getTitle());
+}
+
+@Test
+public void testAddComments() {
+
+    Project newProject = new Project("Flappy Bird", "Make the bird go through pipes for as long as possible");
+
+    Date date = new Date();
+    String text = "Sample Comment";
+    User commentBy = new User("Casey", "Vu", "CaseyVu", "cv10@email.sc.edu", "cv10", "Microsoft", new Date());
+
+    newProject.addComments(date, text, commentBy);
+    ArrayList<Comments> comments = newProject.getTasks.getComments(); //how do i access get comments thats found
+                                                                     //within my tasks within projects? 
+    
+    Comments addedComment = comments.get(0);
+
+    assertEquals(text, addedComment.getText());
+}
+
+@Test
+public void testMoveTasks() {
+    
+    ProjectList projectList = ProjectList.getInstance();
+
+    Date deadline = new Date();
+    String taskDescription = "Sample Task";
+    int priority = 1;
+    double hours = 2.5;
+    User assignedUser = new User("Casey", "Vu", "CaseyVu", "cv10@email.sc.edu", "cv10", "Microsoft", new Date());
+    ArrayList<Change> changes = new ArrayList<>();
+    ArrayList<Comments> comments = new ArrayList<>();
+
+    Project newProject = new Project("Flappy Bird", "Make the bird go through pipes for as long as possible");
+    projectList.addProject(newProject);
+    
+    newProject.addTasks(deadline, taskDescription, priority, hours, assignedUser, changes, comments);
+
+    ArrayList<Tasks> firstColumnTasks = newProject.getColumns().get(0).getTasks();
+    Tasks addedTask = firstColumnTasks.get(0);
+
+    newProject.moveTasks(firstColumnTasks, 0, 1);
+
+    assertFalse(newProject.getColumns().get(0).getTasks().contains(firstColumnTasks));
+}
+
+@Test
+public void testAddChange() {
+
+    ProjectList projectList = ProjectList.getInstance();
+    Project newProject = new Project("Flappy Bird", "Make the bird go through pipes for as long as possible");
+    projectList.addProject(newProject);
+
+    User user = new User("Casey", "Vu", "CaseyVu", "cv10@email.sc.edu", "cv10", "Microsoft", new Date());
+    String description = "Change description";
+    Date initialLastUpdate = projectHistory.getLastUpdate();
+    projectHistory.addChange(user, description);
+
+    Date updatedLastUpdate = projectHistory.getLastUpdate();
+
+    assertNotEquals(initialLastUpdate, updatedLastUpdate);
+}
+
+@Test
+public void testRemoveChange() {
+
+    User user = new User("Casey", "Vu", "CaseyVu", "cv10@email.sc.edu", "cv10", "Microsoft", new Date());
+    String description = "Sample change description";
+    Change change = new Change("Changed the font of the headers");
+
+    projectHistory.getAllChanges().add(change);
+
+    boolean removed = projectHistory.removeChange(change);
+
+    assertFalse(projectHistory.getAllChanges().contains(change));
+}
+
+@Test
 public void testAddProject() {
     
     ProjectList projectList = ProjectList.getInstance();
@@ -109,6 +211,20 @@ public void testAddProject() {
 
     assertEquals(1, projects.size());
 }
+
+@Test
+public void testRemoveProject() {
+    
+    ProjectList projectList = ProjectList.getInstance();
+
+    String nameToRemove = "Project1";
+    String descriptionToRemove = "Description1";
+    boolean removed = projectList.removeProject(nameToRemove, descriptionToRemove);
+
+    assertTrue(removed);
+}
+
+
 
 
 
