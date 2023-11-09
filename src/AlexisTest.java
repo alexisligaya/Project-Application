ae9321b6722b7d185fa3b87838e1527d2371a8a0
alexisligaya <alp12@email.sc.edu>
2023-11-08 21:27:07 -0500

    test methods added

--- src/AlexisTest.java
+++ src/AlexisTest.java
@@ -35,53 +35,71 @@ public class AlexisTest {
     //Alexis- DataWriter, User, UserList, Application
     
     //DataWriter tests
-    
     @Test
+    //tests that list of users saves to json file
     public void testSaveUsers(){
         ArrayList<User> testUsers = new ArrayList<>();
         testUsers.add(testUser);
         File testFile = tempDir.resolve("user-test.json").toFile();
 
+        //saves users to file
         DataWriter.saveUsers(testUsers);
+
+        //checks if file exists
         assertTrue(testFile.exists());
     }
 
     @Test
+    //tests that list of projects svae to json file
     public void testSaveProjects(){
         ArrayList<Project> testProjects = new ArrayList<>();
         testProjects.add(testProject);
         File testFile = tempDir.resolve("project-test.json").toFile();
-
+       
+        //saves projects to file
         DataWriter.saveProjects(testProjects);
+
+        //checks if file exists
         assertTrue(testFile.exists());
     }
 
     //User tests 
     @Test
     public void testJoinTeam() {
+        //tests if user joins team
         testUser.joinTeam(testTeam);
+
+        //checks if user joined list of team members
         assertTrue(testTeam.getScrumTeamMembers().contains(testUser));
     }
     
     @Test
     public void testLeaveTeam() {
+        //user joins and leaves team
         testUser.joinTeam(testTeam);
         testUser.leaveTeam(testTeam);
+
+        //checks that user is not in list of team members
         assertFalse(testTeam.getScrumTeamMembers().contains(testUser));
     }
 
     @Test
     public void testViewProjects(){
+        //user views projects in a team 
         testUser.joinTeam(testTeam);
         testTeam.addProject(testProject);
         testProject.addMember(testUser);
         
+        //project that user can view
         ArrayList<Project> projects = testUser.viewProjects();
+
+        //check that project is in the list
         assertTrue(projects.contains(testProject));
     }
 
     @Test
     public void testIsOnline(){
+        //sets user as online
         UUID userID = testUser.getUserID();
         UserList.getInstance().setUserOnline(userID, true);
         boolean isOnline = testUser.isOnline();
@@ -91,21 +109,28 @@ public class AlexisTest {
     //UserList tests
     @Test
     public void testAddUser(){
+        //adds user to the user list
         assertTrue(userList.addUser("Alexis", "Peters", "APeters", "ap@gmail.com", "12345", "UofSC", new Date()));
+       
+       //checks that the added user is online
         assertTrue(userList.isUserOnline(userList.getUsers().get(0).getUserID()));
     }
 
     @Test
     public void testSetUserOnline(){
+        //sets user an online
         assertTrue(userList.addUser("Alexis", "Peters", "APeters", "ap@gmail.com", "12345", "UofSC", new Date()));
         UUID userID = userList.getUsers().get(0).getUserID();
         userList.setUserOnline(userID, true);
+
+        //chekcs that the user is online
         assertTrue(userList.isUserOnline(userID));
     }
 
     //Applciation tests
     @Test
     public void testSignUp(){
+        //test user info
         String firstName = "Alexis";
         String lastName = "Peters";
         String userName = "APeters";
@@ -114,15 +139,28 @@ public class AlexisTest {
         String company = "UofSC";
         Date dateOfBirth = new Date();
 
+        //checks for succesfull signup
         boolean signUp = application.signUp(firstName, lastName, userName, email, password, company, dateOfBirth);
         assertTrue(signUp);
     }
 
     @Test
+    public void testDuplicateUser(){
+        //creates two new users with the same username and password
+        boolean user1 = userList.addUser("Alexis", "Peters", "APeters", "APeters@gmail.com", "12345", "UofSC", new Date());
+        boolean user2 = userList.addUser("Alexis", "Peters", "APeters", "APeters@gmail.com", "12345", "UofSC", new Date());
+
+        assertTrue(user1);
+        assertFalse(user2);
+    }
+
+    @Test
     public void testLogin(){
+        //user login info
         String userName = "APeters";
         String password = "12345";
 
+        //checks for successfull login
         boolean login = application.login(userName, password);
         assertTrue(login);
     }
@@ -134,22 +172,38 @@ public class AlexisTest {
         ArrayList<User> savedUsers = application.getUserList().getUsers();
         ArrayList<Project> savedProjects = application.getProjectList().getProjects();
 
-
+        //user logs out
         application.logout();
 
+        //loads user and projects
         application.getUserList().loadUsers();
         application.getProjectList().loadProjects();
+
+        //checks that there are no changes to user and project list
         assertEquals(currUsers, savedUsers);
         assertEquals(currProjects, savedProjects);
     }
 
     @Test
+    public void testLoginToLogout(){
+        //user logs out
+        application.logout();
+        //user logs in again
+        boolean loginToLogout = application.login("APeters", "12345");
+        assertTrue(loginToLogout);
+    }
+
+    @Test
     public void testAddProject(){
+        //new project info
         Project newProject = new Project("project", "description");
 
+        //project added 
         application.addProject(newProject);
 
         ArrayList<Project> projects = application.getProjectList().getProjects();
+
+        //checks whether new project is in the list
         assertTrue(projects.contains(newProject));
     }
 
@@ -157,12 +211,12 @@ public class AlexisTest {
     public void testRemoveProject(){
         Project newProject = new Project("project", "description");
 
+        //adds then removes project
         application.addProject(newProject);
-
         boolean removed = application.removeProject("project", "description");
 
+        //checks whether project was removed from project list
         assertTrue(removed);
-
         ArrayList<Project> projects = application.getProjectList().getProjects();
         assertFalse(projects.contains(newProject));
     }
