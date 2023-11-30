8c685dd9201ac135e670283d22698e6a87489d6a
alexisligaya <alp12@email.sc.edu>
2023-11-30 17:30:05 -0500

    gui changes

--- src/demo/src/main/java/com/example/LoginController.java
+++ src/demo/src/main/java/com/example/LoginController.java
@@ -7,11 +7,7 @@ import javafx.fxml.FXML;
 public class LoginController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
-    @FXML
     private void switchToDashboard() throws IOException {
-        App.switchToDashboard();
+        App.setRoot("dashboard");
     }
 }
