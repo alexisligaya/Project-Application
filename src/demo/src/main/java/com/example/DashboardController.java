ea3fbef0f0918e64590eb0f621b68dd71a1d723d
alexisligaya <alp12@email.sc.edu>
2023-12-03 15:53:08 -0500

    worked on screens

--- src/demo/src/main/java/com/example/DashboardController.java
+++ src/demo/src/main/java/com/example/DashboardController.java
@@ -3,12 +3,14 @@ package com.example;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class DashboardController {
 
+
     @FXML
     private void switchToOpenProject() throws IOException {
-        App.setRoot("Project 1");
+        App.setRoot("openProject");
     }
 
 }
